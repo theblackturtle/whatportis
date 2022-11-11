07145f19d754573308b042c00abdce3589c5c67e
theblackturtle <1998.viettuan@gmail.com>
2022-11-11 09:57:45 +0700

    .

--- setup.py
+++ setup.py
@@ -1,52 +1,37 @@
 from setuptools import setup
 
 entry_points = {
-    'console_scripts': [
-        'whatportis=whatportis.cli:run',
+    "console_scripts": [
+        "whatportis=whatportis.cli:run",
     ]
 }
-readme = open('README.rst').read()
+readme = open("README.rst").read()
 
 setup(
     name="whatportis",
-    version="0.8.2",
-    url='http://github.com/ncrocfer/whatportis',
-    author='Nicolas Crocfer',
-    author_email='ncrocfer@gmail.com',
+    version="0.8.3",
+    url="http://github.com/theblackturtle/whatportis",
+    author="Nicolas Crocfer",
+    author_email="ncrocfer@gmail.com",
     description="A command to search port names and numbers",
     long_description=readme,
-    packages=['whatportis'],
+    packages=["whatportis"],
     include_package_data=True,
-    install_requires=[
-        "simplejson==3.17.2",
-        "tinydb==4.4.0",
-        "requests==2.25.1",
-        "prettytable==2.1.0",
-        "click==7.1.2"
-    ],
-    extras_require={
-        "dev": [
-            "pytest",
-            "tox",
-            "black"
-        ],
-        "server": [
-            "flask==1.1.2"
-        ]
-    },
+    install_requires=["simplejson==3.17.2", "tinydb==4.4.0", "requests==2.25.1", "prettytable==2.1.0", "click==7.1.2"],
+    extras_require={"dev": ["pytest", "tox", "black"], "server": ["flask==1.1.2"]},
     entry_points=entry_points,
     classifiers=(
-        'Development Status :: 5 - Production/Stable',
-        'Environment :: Console',
-        'Natural Language :: English',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9'
+        "Development Status :: 5 - Production/Stable",
+        "Environment :: Console",
+        "Natural Language :: English",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
     ),
 )
